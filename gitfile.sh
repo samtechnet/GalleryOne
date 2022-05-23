90129a6990c36ba2d3e438ca2a6d2453c5dc3124
Pere_ebi Egbekun <pereebiegbekun@gmail.com>
2022-05-23 04:12:51 +0100

    committing Products

--- gitfile.sh
+++ gitfile.sh
@@ -6,6 +6,6 @@ git add .
 echo "I'm adding all file here"
 git commit -m "products folder"
 echo "last line of script"
-git branch -m Pere
-git push -u origin Pere
+git branch -m Dev
+git push -u origin Dev
 echo "                         Done"
