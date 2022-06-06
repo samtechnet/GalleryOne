845d0406109d10a27f57b4d5296fdfd060600f7c
Oladimeji Shekoni <dimejishekoni@gmail.com>
2022-06-06 02:06:00 +0100

    First Commit

--- scripts/create_dataset.py
+++ scripts/create_dataset.py
@@ -5,8 +5,8 @@ import numpy as np
 import pandas as pd
 
 num_users = 100000
-features = ['ID','Gender','Occupation_Status','Age','State','Salary','debt_to_debt',
-    'Current_Loan','Tenure','Previous_Loans','Defaulted','Default_Dur']
+features = ['ID','Gender','Marital_Status', 'Dependants', 'Education','Occupation_Status','Age',\
+            'State','State_Code', 'Salary','Current_Loan','Tenure','Previous_Loans','Defaulted','Default_Dur']
 df = pd.DataFrame(columns = features)
 
 class Create_Dataset: 
@@ -23,14 +23,14 @@ class Create_Dataset:
         return self.df
     
     def create_gender(self) -> pd.DataFrame:
-        genders = ["male", "female", "na"]
+        genders = ["male", "female"]
         self.df['Gender'] =  random.choices(
-            genders, weights=(70,24,5), k=num_users)
+            genders, weights=(60,40), k=num_users)
         return self.df
     
     def create_occupation(self) -> pd.DataFrame:
         occupation = ['Employed', 'Self Employed', 'Unemployed']
-        self.df['Occupation Status'] = random.choices(
+        self.df['Occupation_Status'] = random.choices(
             occupation, weights=(35, 55, 10), k=num_users)
         return self.df
 
@@ -61,74 +61,110 @@ class Create_Dataset:
         )
         return self.df
 
-    def create_debt_to_debt_ratio(self) -> pd.DataFrame:
+    def create_state_code(self) -> pd.DataFrame:
         def d2d_ratio(state):
             if (state == 'Abuja') | (state == 'Lagos') | (state == 'Port Harcourt'):
                 return 5
-            elif (state == 'Kano') | (state == 'Kwara') | (state == 'Oyo') | (state == 'Ogun') | (state == 'Cross River' )| (state == 'Imo'):
+            elif (state == 'Kano') | (state == 'Kwara') | (state == 'Oyo') | (state == 'Ogun') | (state == 'Cross River') | (state == 'Imo'):
                 return 4
-            elif (state == 'Kaduna') | (state == 'Kogi') | (state == 'Abia') | (state ==  'Enugu') | (state == 'Osun' )| (state == 'Edo'):
+            elif (state == 'Kaduna') | (state == 'Kogi') | (state == 'Abia') | (state ==  'Enugu') | (state == 'Osun' ) | (state == 'Edo'):
                 return 3
             return 2
         
-        self.df['debt_to_debt'] = [d2d_ratio(i) for i in self.df['State']]
+        self.df['State_Code'] = [d2d_ratio(i) for i in self.df['State']]
         return self.df
+        
     def create_tenure(self) -> pd.DataFrame:
         np.random.seed(3)
         def ten(period):
             if period == True:
                 return np.random.randint(1, 5)
             return 0
-        self.df['Tenure'] = [ten(i) for i in self.df['Current Loan']]
+        self.df['Tenure'] = [ten(i) for i in self.df['Current_Loan']]
         return self.df
 
     def create_current_loan(self) -> pd.DataFrame: 
         choice = [True, False]
-        self.df['Current Loan'] = random.choices(
+        self.df['Current_Loan'] = random.choices(
             choice, weights = (30, 70),k=num_users)
         return self.df
 
-    def create_previous_loans(self) -> pd.DataFrame:
-        return ...
+    def create_previous_loans(self) -> pd.DataFrame:#
+        self.df['Previous_Loans'] = self.df['Current_Loan'].apply(lambda x: random.choices((1,2,3,4), weights = [50,25,15,10])[0] if x == True else random.choices((0,1,2), weights = [60,32,8])[0])
+        return self.df
 
     def create_salary(self) -> pd.DataFrame: 
-        return ... 
-    def create_defaulted(self) -> pd.DataFrame: 
-        return ... 
-    def create_defaulted_rat(self) -> pd.DataFrame: 
-        return ... 
-    
-    def create_data(self, save = False) -> pd.DataFrame:
-        """Required columns to be created"""
-        features = ['ID','Gender','Occupation_Status','Age','State','Salary','debt_to_debt',\
-                    'Current_Loan','Tenure','Previous_Loans','Defaulted','Default_Dur']
-
-        self.df['ID'] = self.create_unique_id()
-        self.df['Gender'] = self.create_gender()
-        self.df['Occupation_Status'] = self.create_occupation()
-        self.df['Age'] = self.create_age()
-        self.df['State'] = self.create_state()
-        self.df['debt_to_debt'] = self.create_debt_to_debt_ratio()
-        self.df['Current_Loan'] = self.create_current_loan()
-        self.df['Tenure'] = self.create_tenure()
-        self.df['Previous_Loans'] = self.create_previous_loans()
-        self.df['Salary'] = self.create_salary()
-        self.df['Defaulted'] = self.create_defaulted()
-        self.df['Default_Dur'] = self.create_defaulted_rat()
+        def salary(df, sal, stats, loc, scale):
+            self.df.loc[(self.df['State_Code'] == sal) & (self.df['Occupation_Status'] == stats),'Salary'] =  int(np.random.normal(loc = loc, scale = scale))
+        salary(self.df, 5,'Employed',300000,150000)
+        salary(self.df, 5,'Self Employed',150000,50000)
+        salary(self.df, 5,'Unemployed',20000,5000)
         
-        data = zip(self.df['ID'],self.df['Gender'],self.df['Occupation_Status'],self.df['Age'],self.df['State'],self.df['Salary'],self.df['debt_to_debt'],
-            self.df['Current_Loan'],self.df['Tenure'],self.df['Previous_Loans'],self.df['Defaulted'],self.df['Default_Dur'])
-        self.df = pd.DataFrame(data = data, columns=features)
+        salary(self.df, 4,'Employed',200000,75000)
+        salary(self.df, 4,'Self Employed',100000,30000)
+        salary(self.df, 4,'Unemployed',10000,5000)
+
+        salary(self.df, 3,'Employed',100000,50000)      
+        salary(self.df, 3,'Self Employed',50000,30000)
+        salary(self.df, 3,'Unemployed',10000,5000)
+
+        salary(self.df, 2,'Employed',60000,30000)
+        salary(self.df, 2,'Self Employed',20000,10000)
+        salary(self.df, 2,'Unemployed',5000,3000)
+        return self.df 
+
+    def create_defaulted(self) -> pd.DataFrame: 
+        self.df['Defaulted'] = self.df['Previous_Loans'].apply(lambda x : random.choices([True, False], weights = [40, 60])[0] if x >= 1 else False)
+        return self.df
+
+    def create_defaulted_dur(self) -> pd.DataFrame: 
+        self.df['Default_Dur'] = self.df['Defaulted'].apply(lambda x : random.choices([1,2,3,4], weights = (10,7,4,3))[0] if x == True else 0)
+        return self.df
+    
+    def create_education(self) -> pd.DataFrame: 
+        education = ['Graduate', 'No Graduate']
+        self.df['Education'] = self.df['Age'].apply(lambda x : random.choices(education, weights = [60, 40])[0] if x >= 27 else ('No Graduate' if x < 22 else random.choices(education, weights = [50,50])[0]))
+        return self.df
+    
+    def create_marriage_stats(self) -> pd.DataFrame:
+        married_stats = ['Married', 'Not Married']
+        def mar_stats(df, num_2, gen, choice): 
+            self.df.loc[(self.df['Age'] <= num_2) & (self.df['Gender'] == gen), 'Marital_Status'] = choice
+        def mar_stats_2(df, num_2, gen, choice):
+            self.df.loc[(self.df['Age'] > num_2) & (self.df['Gender'] == gen), 'Marital_Status'] = choice
+
+        mar_stats(self.df, 26, 'Male', random.choices(married_stats, weights =[30,70], k = len(self.df.loc[(self.df.Age <=26) & (self.df.Gender == 'Male')])))
+        mar_stats(self.df, 25, 'Female', random.choices(married_stats, weights =[50,50], k = len(self.df.loc[(self.df.Age <= 25) & (self.df.Gender == 'Female')])))
+        mar_stats_2(self.df, 26, 'Male', random.choices(married_stats, weights =[70, 30], k = len(self.df.loc[(self.df.Age > 26) & (self.df.Gender == 'Male')])))
+        mar_stats_2(self.df, 25, 'Female', random.choices(married_stats, weights =[80,20], k = len(self.df.loc[(self.df.Age > 25) & (self.df.Gender == 'Female')])))
+        return self.df
+
+    def create_dependants(self) -> pd.DataFrame:
+        self.df['Dependants'] = self.df['Marital_Status'].apply(lambda x : np.random.randint(0,6) if x == 'Married' else np.random.randint(0,2))
+        return self.df
+
+    def create_data(self, save = False) -> pd.DataFrame:
+        self.create_unique_id()
+        self.create_gender()
+        self.create_occupation()
+        self.create_age()
+        self.create_state()
+        self.create_state_code()
+        self.create_current_loan()
+        self.create_tenure()
+        self.create_previous_loans()
+        self.create_salary()
+        self.create_defaulted()
+        self.create_defaulted_dur()
+        self.create_marriage_stats()
+        self.create_education()
+        self.create_dependants()
+        
         if save:
             self.df.to_csv('../data/auction_data.csv', index=False)
             print('File Successfully Saved.!!!')
         return self.df
 
 if __name__ == "__main__":
-    """Required columns to be created"""
-    features = ['ID','Gender','Occupation_Status','Age','State','Salary','debt_to_debt',
-        'Current_Loan','Tenure','Previous_Loans','Defaulted','Default_Dur']
-    # df = pd.read_csv("/home/codeally/project/Twitter-Data-Analysis/data/processed_tweet_data.csv") 
     cleaner = Create_Dataset(df)
     cleaner.create_data(True)
-    # def create_previous_loans(self) -> pd.DataFrame: 
