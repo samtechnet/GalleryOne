e88fd0bb5959dfce1b76ebbf6bbfaff4b5d9c67c
samuel omolaja <87916045+samtechnet@users.noreply.github.com>
2022-06-22 18:00:03 +0100

    delete

--- src/Error/ApiError.ts
+++
@@ -1,9 +0,0 @@
-export class AppError extends Error {
-    constructor(public message: string, public statusCode: number) {
-        super(message);
-        this.statusCode = statusCode;
-        // this.isOperational =  true;
-
-        Error.captureStackTrace(this, this.constructor);
-    }
-}
--- src/Error/catchAsync.ts
+++
@@ -1,5 +0,0 @@
-import express, { application, NextFunction, Request, Response } from 'express';
-
-// @ts-ignore
-export const catchErrors = (fn) => (req: Request, res: Response, next: NextFunction) => 
-    fn(req, res, next).catch(next);
